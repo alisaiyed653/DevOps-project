8b018729ef0fc0aa8618749bdc3f94266b2c799b
alisaiyed653 <158357233+alisaiyed653@users.noreply.github.com>
2024-11-05 14:34:22 +0000

    Add files via upload
    updated version of terraform, which deploys security group and ec2 for jenkins and installs all dependencies required for jenkins on amazon linux machine.

--- main.tf
+++ main.tf
@@ -74,6 +74,66 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Security Group for Jenkins EC2 Instance
+resource "aws_security_group" "allow_jenkins" {
+  name        = "allow_jenkins"
+  description = "Allow SSH and HTTP for Jenkins"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere (consider restricting this for production)
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access to Jenkins
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance for Jenkins
+resource "aws_instance" "jenkins_instance" {
+  ami           = "ami-0acc77abdfc7ed5a6" 
+  instance_type = "t2.micro" 
+  subnet_id     = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.allow_jenkins.id]
+  key_name = "jenkins"
+  tags = {
+    Name = "Jenkins-Instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y java-17-amazon-corretto
+              sudo yum install -y docker
+              systemctl start docker
+              systemctl enable docker
+              sudo yum install -y python3
+              sudo yum install -y git
+              sudo pip3 install --upgrade pip
+              # Install Jenkins
+              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+              sudo yum install -y jenkins
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+              EOF
+}
+
+# IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution" {
   name = "ecsTaskExecutionRole"
 
@@ -99,29 +159,24 @@ resource "aws_ecs_cluster" "my_ecs_cluster" {
   name = "my-flask-app-cluster"
 }
 
-
 # ECS Task Definition
 resource "aws_ecs_task_definition" "flask_task" {
   family                   = "flask-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"  # Adjust as needed
-  memory                   = "512"  # Adjust as needed
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([
-    {
+  container_definitions = jsonencode([{
       name      = "flask-container"
       image     = "255625225972.dkr.ecr.eu-west-2.amazonaws.com/flask-web-app:latest"
       essential = true
-      portMappings = [
-        {
+      portMappings = [{
           containerPort = 80
           hostPort      = 80
-        }
-      ]
-    }
-  ])
+      }]
+  }])
 }
 
 # ECS Service
@@ -131,6 +186,7 @@ resource "aws_ecs_service" "flask_service" {
   task_definition = aws_ecs_task_definition.flask_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+
   network_configuration {
     subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
     security_groups = [aws_security_group.allow_http.id]
